7515882ab1ed86ec9192ad1e611e45c9d924ec2f
Ngaukon <josephlucia100@gmail.com>
2024-06-22 16:03:25 -0700

    filter state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This is a script that lists all states from the
-database `hbtn_0e_0_usa`.
+This script lists all states with
+a `name` starting with the letters `N`
+from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -10,13 +11,15 @@ from sys import argv
 if __name__ == '__main__':
     """
     Access to the database and get the states
-    from the database.
+    from the made database.
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
