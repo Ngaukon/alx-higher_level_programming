86a4ade6ba9225cf0c94b8fd0e68bb22626788a8
Ngaukon <josephlucia100@gmail.com>
2024-07-30 13:19:58 -0700

    complet tasks update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
-'use strict';
+
 const request = require('request');
-const fs = require('fs')
+const url = process.argv[2];
 
-request(process.argv[2], { json: true }, (error, response, data) => {
-  if (error) console.log(error); else {
-    if (response.statusCode === 200) {
-      const todos = {};
-
-      data.forEach(todo => {
-        if (todos[todo.userId] === undefined) todos[todo.userId] = 0;
-        if (todo.completed === true) todos[todo.userId]++;
-      });
-      console.log(todos);
-    }
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
 });
