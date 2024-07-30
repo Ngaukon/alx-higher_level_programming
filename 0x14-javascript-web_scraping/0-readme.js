3cd8537d4bc8fcc2343fe9365fe8dc70145b6d38
Ngaukon <josephlucia100@gmail.com>
2024-07-30 13:04:41 -0700

    update the readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,11 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
 
-if (!filePath) {
-  console.error('Usage: ./0-readme.js <file path>');
-  process.exit(1);
-}
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
+fs.readFile(process.argv[2], 'utf-8', (error, responce) => {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data);
+    console.log(responce);
   }
 });
-
